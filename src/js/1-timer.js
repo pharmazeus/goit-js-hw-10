d43c558c8a61f44ad57be05c2f68fd4a41e8a2d9
Pharma.zeus <markmaydeens@gmail.com>
2025-01-10 11:51:29 -0500

    try

---
+++ src/js/1-timer.js
@@ -0,0 +1,18 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
